61046e41e31fe9e9916532f6ef402c3702d360b9
rnicrosoft <rnicrosoft@live.cn>
2024-11-11 18:31:22 +0800

    fix(pref): deps issue

--- src/modules/pref.ts
+++ src/modules/pref.ts
@@ -5,13 +5,11 @@ import { UIFactory } from "./ui";
 export class PrefFactory {
   // 注册设置页面
   static registerPrefs() {
-    const prefOptions = {
+    Zotero.PreferencePanes.register({
       pluginID: config.addonID,
       src: rootURI + "chrome/content/preferences.xhtml",
       label: getString("prefs-title"),
       image: `chrome://${config.addonRef}/content/icons/favicon.png`,
-      defaultXUL: true,
-    };
-    ztoolkit.PreferencePane.register(prefOptions);
+    });
   }
 }
